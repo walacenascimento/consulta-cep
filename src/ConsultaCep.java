29c43d2419c8b0efda868f122276bd19ffc4b537
Walace Nascimento <wlacens@hotmail.com>
2025-05-05 23:22:57 -0300

    Criando a classe ConsultaCep, e instancinado na Main

---
+++ src/ConsultaCep.java
@@ -0,0 +1,21 @@
+import com.google.gson.Gson;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.http.HttpClient;
+import java.net.http.HttpRequest;
+import java.net.http.HttpResponse;
+
+public class ConsultaCep {
+    public Endereco buscacep(String cep) throws IOException, InterruptedException {
+
+        String endereco = "https://viacep.com.br/ws/" + cep + "/json/";
+
+        HttpClient client = HttpClient.newHttpClient();
+        HttpRequest request = HttpRequest.newBuilder().uri(URI.create(endereco)).build();
+
+        HttpResponse<String> response = client.send(request, HttpResponse.BodyHandlers.ofString());
+
+        return new Gson().fromJson(response.body(), Endereco.class);
+    }
+}
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,13 @@
+import java.io.IOException;
+
 public class Main {
-    public static void main(String[] args) {
+    public static void main(String[] args) throws IOException, InterruptedException {
         System.out.println("Hello, World!");
+
+        ConsultaCep consultaCep = new ConsultaCep();
+
+        Endereco endereco = consultaCep.buscacep("45656502");
+
+        System.out.println(endereco);
     }
 }
