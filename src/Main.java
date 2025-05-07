ec8eda1558af834f01328dd16f8e6747100ede25
Walace Nascimento <wlacens@hotmail.com>
2025-05-06 21:28:15 -0300

    implementando a classe Scanner na classe Main, para deixar o código mais dinâmico

--- src/Main.java
+++ src/Main.java
@@ -1,13 +1,21 @@
 import java.io.IOException;
+import java.util.Scanner;
 
 public class Main {
     public static void main(String[] args) throws IOException, InterruptedException {
-        System.out.println("Hello, World!");
 
+        System.out.println("digite um cep para efetuar a busca: ");
+        Scanner leitura = new Scanner(System.in);
+
+        String cep = leitura.nextLine();
         ConsultaCep consultaCep = new ConsultaCep();
 
-        Endereco endereco = consultaCep.buscacep("45656502");
+        try {
+            Endereco endereco = consultaCep.buscacep(cep);
+            System.out.println(endereco);
+        } catch (Exception e){
+            System.out.println(e.getMessage());
+        };
 
-        System.out.println(endereco);
     }
 }
